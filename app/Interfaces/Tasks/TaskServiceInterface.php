0569a305ae210f5f4cae6e70c75af83e39a7215a
Mateo Saldarriaga <teo0917s@gmail.com>
2024-11-27 22:12:29 -0500

    Finished interfaces

---
+++ app/Interfaces/Tasks/TaskServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Interfaces\Tasks;
+
+interface TaskServiceInterface
+{
+    public function getAllTasks();
+    public function getTaskById($id);
+    public function createTask(array $data);
+    public function updateTask($id, array $data);
+    public function deleteTask($id);
+}
